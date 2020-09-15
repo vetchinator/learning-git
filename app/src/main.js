2518b8f77418ab6b176581f2604250df5dd57e87
vetchinator <55415208+vetchinator@users.noreply.github.com>
2020-09-15 13:13:01 +0300

    Добавлен Dev server

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue';
 
-createApp(App).mount('#app')
+import Vue from 'vue';
+
+
+Vue.config.productionTip = false;
+Vue.config.devtools = process.env.NODE_ENV === 'development';
+Vue.config.devtools = true;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
